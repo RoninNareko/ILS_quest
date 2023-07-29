fe4f7d15fb244f691b003853f086a862ce5f9cf0
narekhasratyan <hasratyan.2000@gmail.com>
2023-07-29 14:40:00 +0400

    todo2

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -10,31 +10,33 @@ import styles from "./Map.module.scss";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import RoutingMachine from "./RoutingMachine";
 
 const position = [59.84660399, 30.29496392];
+
+const routePoints = [
+  { latitude: 59.84660399, longitude: 30.29496392 },
+  { latitude: 59.82934196, longitude: 30.42423701 },
+  { latitude: 59.83567701, longitude: 30.38064206 },
+];
 
 function Map() {
   const cx = classNames.bind(styles);
   const [route, setRoute] = useState([]);
 
   useEffect(() => {
-    // Define the coordinates of the start and end points
-    const routePoints = [
-      { latitude: 59.84660399, longitude: 30.29496392 },
-      { latitude: 59.82934196, longitude: 30.42423701 },
-      { latitude: 59.83567701, longitude: 30.38064206 },
-    ];
-
-    // Fetch the route data from the OSRM API
     const fetchData = async () => {
       try {
         // @ts-ignore
-        const coordinates = routePoints
-          .map((point) => `${point.longitude},${point.latitude}`)
+        const coordinatesString = routePoints
+          .map((point) => `${point.latitude},${point.longitude}`)
           .join(";");
+
         const response = await axios.get(
-          `https://router.project-osrm.org/route/v1/driving/${coordinates}?overview=full&geometries=geojson`
+          `http://router.project-osrm.org/route/v1/driving/${coordinatesString}?overview=false`
         );
+        console.log("response", response.data.waypoints);
+
         const routeCoordinates = response.data.routes[0].geometry.coordinates;
         setRoute(routeCoordinates);
       } catch (error) {
@@ -58,12 +60,14 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position as any}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {/* {routePoints.map((point, index) => (
+          <Marker position={[point.latitude, point.longitude]} key={index}>
+            <Popup>{`Point ${index + 1}`}</Popup>
+          </Marker>
+        ))} */}
         {route && <Polyline pathOptions={limeOptions} positions={route} />}
+        <RoutingMachine />
       </MapContainer>
     </section>
   );
